f05b73f91619ee6642eb5cf0fba9734fa9efcefc
juliosalag <juliosalag@gmail.com>
2022-02-14 12:54:03 +0100

    Primer API REST con Express Parametizable

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict'
+const port = process.env.PORT || 8888;
 const express = require('express');
 const app = express();
 
-app.get('/hola', (request, response) => {
-    response.send('Hola a todas y a todos desde Express!')
+app.get('/hola/:unNombre', (req, res) => {
+    res.status(200).send({ mensaje: `Hola ${req.params.unNombre} desde SD!` });
 });
 
-app.listen(8080, () => {
-    console.log('API REST ejecutándose en http://localhost:8080/hola');
+app.listen(port, () => {
+    console.log(`API REST ejecutándose en http://localhost:${port}/hola/:unNombre`);
 });
