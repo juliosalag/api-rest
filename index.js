20201b6a9cc2b098023c8e9bc8fa2303a9ff9b88
juliosalag <juliosalag@gmail.com>
2022-02-14 12:48:38 +0100

    Primer servidor HTTP

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var http = require('http');
+var server = http.createServer();
 
-http.createServer( (request, response) => {
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.end('Hola a todas y a todos!\n');
-}).listen(8080);
+function HTTP_Response(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write('Hola a todas y a todos!\n');
+    response.end();
+}
 
+server.on('request', HTTP_Response);
+server.listen(8080);
 console.log('Servidor ejecutándose en puerto 8080...');
-
