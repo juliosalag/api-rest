c83cd9311677e30f7aa8cf101899f695625d752c
juliosalag <juliosalag@gmail.com>
2022-02-14 16:21:12 +0100

    Primer API REST con Express y JSON

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const port = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
 
+// Declaramos los middleware
+app.use(logger('dev'));
+
+// Declaramos el API
 app.get('/hola/:name', (req, res) => {
     res.status(200).send({ mensaje: `Hola ${req.params.name} desde SD con JSON!` });
 });
